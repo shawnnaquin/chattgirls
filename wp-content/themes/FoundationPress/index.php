68eaca6796fa24cd6546c43492e0cdeb51925592
shawnnaquin <shawn.naquin@gmail.com>
2016-02-07 16:07:44 -0500

    reset index.php

--- wp-content/themes/FoundationPress/index.php
+++ wp-content/themes/FoundationPress/index.php
@@ -13,16 +13,14 @@
  * @subpackage FoundationPress
  * @since FoundationPress 1.0.0
  */
-
 get_header(); ?>
 
-<!-- <div id="page" role="main">
-	<article class="main-content"> -->
-
-	<?php/* if ( have_posts() ) : ?>
+<div id="page" role="main">
+	<article class="main-content">
+	<?php if ( have_posts() ) : ?>
 
 		<?php /* Start the Loop */ ?>
-		<?php /* while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'content', get_post_format() ); ?>
 		<?php endwhile; ?>
 
@@ -32,15 +30,14 @@ get_header(); ?>
 		<?php endif; // End have_posts() check. ?>
 
 		<?php /* Display navigation to next/previous pages when applicable */ ?>
-		<?php /* if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+		<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
 			<nav id="post-nav">
 				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
 				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
 			</nav>
-		<?php } */ ?>
+		<?php } ?>
 
 	</article>
-
 	<?php get_sidebar(); ?>
 
 </div>
